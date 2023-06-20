f96e706baa46a6278731c2c39c0ccf7ad87f2202
seeyoo <lukdown10@gmail.com>
2023-06-20 15:28:42 +0900

    .,

--- Day01/ex06.py
+++ Day01/ex06.py
@@ -19,3 +19,32 @@ print('li2[:5] : ',li2[:5])
 
 print('li2[5:7] : ',li2[5:7])
 print('li2[5:] : ',li2[5:])
+
+
+# 요소 추가 및 삭제
+# - 요소 추가 : append(), insert()
+# - 요소 삭제 : pop()
+
+li3 = [1,2,3,4,5]
+# append(요소)              : 마지막에 요소 추가
+print('### append() ###')
+li3.append(6)
+print(li3)
+
+li3.append(7)
+print(li3)
+
+# insert(인덱스,요소)       : 인덱스에 요소 추가
+print('### insert() ###')
+li3.insert(2, 2.5)
+print(li3)
+
+# pop(인덱스)               : 인덱스의 요소 삭제
+# pop()                     : 마지막 요소 삭제
+print('### pop() ###')
+li3.pop(2)
+print(li3)
+
+li3.pop()
+li3.pop()
+print(li3)
