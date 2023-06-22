643bac2e6ed7397b9489a4641f8b29a3094bcfca
seeyoo <lukdown10@gmail.com>
2023-06-22 11:36:08 +0900

    .,.,.,

--- human_Python/Day03/ex03.py
+++ human_Python/Day03/ex03.py
@@ -1,15 +1,5 @@
 
-def secure_name(name):
-    cnt = len(name) - 1
-    return name[0] + ('*' * cnt)
-
-
-def secure_no(no):
-    return no[0:8] + '******'
-
-def secure_phone(phone):
-    return phone.replace( phone[4:8], '****' )
-    
+from secure import *
 
 # 사용자 정보 마스킹하기
 name = '김휴먼'
