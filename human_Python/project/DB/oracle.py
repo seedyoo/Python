aeb86f983bfb5b8a07dfd36056014716e84b2d78
seeyoo <lukdown10@gmail.com>
2023-06-23 14:39:30 +0900

    .,,.

--- human_Python/project/DB/oracle.py
+++
@@ -1,9 +0,0 @@
-import cx_Oracle
-
-# 데이터베이스 연결 정보
-dsn = cx_Oracle.makedsn(host='localhost', port='1521', sid='orcl')
-username = 'human'
-password = '123456'
-
-# 데이터베이스 연결
-connection = cx_Oracle.connect(username, password, dsn)
